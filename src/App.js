bb241056f5bb19a4e2c53a236f08085977df84e6
Zack C <claar.zack@gmail.com>
2020-11-12 15:39:28 -0600

    install react-router-dom. All components working and showing in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Landing from './components/landing/Landing';
+import Products from './components/products/Products';
+import Cart from './components/cart/Cart';
+
 import './App.css';
+
 
 function App() {
   return (
     <div>
-      APP!
+      <Header />
+      <Landing />
+      <Products />
+      <Cart />
+      <Footer />
     </div>
   );
 }
